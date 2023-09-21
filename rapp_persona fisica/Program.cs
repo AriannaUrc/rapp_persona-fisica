6a617f6a5240d151f41a92f918a6f5c5bb703126
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2023-09-21 10:52:15 +0200

    Aggiungere i file di progetto.

---
+++ rapp_persona fisica/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rapp_persona_fisica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Persona persona = new Persona();
+            Console.WriteLine(persona.Mostra());
+        }
+
+        class Persona
+        {
+            private int codice;
+            private string nome;
+            private string cognome;
+            private bool registrato;
+
+            public Persona(int codice, string nome, string cognome, bool registrato)
+            {
+                this.codice = codice;
+                this.nome = nome;
+                this.cognome = cognome;
+                this.registrato = registrato;
+            }
+
+            public Persona()
+            {
+                this.codice = 0;
+                this.nome = "none";
+                this.cognome = "none";
+                this.registrato = false;
+            }
+
+
+            public void SetCodice(int codice)
+            {
+                this.codice = codice;
+            }
+
+            public int GetCodice()
+            {
+                return codice;
+            }
+
+            public void SetNome(string nome)
+            {
+                this.nome = nome;
+            }
+
+            public string GetNome()
+            {
+                return this.nome;
+            }
+
+            public void SetCognome(string cognome)
+            {
+                this.cognome = cognome;
+            }
+
+            public string GetCognome()
+            {
+                return this.cognome;
+            }
+
+            private void SetRegistrato(bool registrato)
+            {
+                this.registrato = registrato;
+            }
+
+            public bool GetRegistrato()
+            {
+                return registrato;
+            }
+
+
+
+            public void Registra()
+            {
+                SetRegistrato(true);
+            }
+
+            public string Mostra()
+            {
+                return "\nCodice: " + codice + "\nNome: " + nome + "\nCognome: " + cognome + "\nRegistrato: " + registrato;
+            }
+        }
+    }
+}
